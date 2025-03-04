aa5dc85a7fb0f6058d08453de6e358c64c9c6563
배재혁 <jhb10109@gmail.com>
2025-03-04 12:32:56 +0900

    [fix] 기능구현을 위한 작업 수정

--- src/main/java/fitmeup/entity/TrainerScheduleEntity.java
+++ src/main/java/fitmeup/entity/TrainerScheduleEntity.java
@@ -1,3 +1,72 @@
+
+
+package fitmeup.entity;
+
+import fitmeup.dto.TrainerScheduleDTO;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="trainer_schedule")
+@Entity
+public class TrainerScheduleEntity {
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Integer trainerScheduleId;
+	    
+		@Builder.Default
+		@Column(name="trainer_id")
+		private Integer trainerId = 1;
+		
+		 @Column(name = "start_time", nullable = false)
+	    private LocalDateTime startTime;
+		 @Column(name = "end_time", nullable = false)
+	    private LocalDateTime endTime;
+
+	    // DTO->Entity 변환 메서드
+	    public static TrainerScheduleEntity toEntity(TrainerScheduleDTO trainerScheduleDTO) {
+	        return TrainerScheduleEntity.builder()
+	        	.trainerScheduleId(trainerScheduleDTO.getTrainerScheduleId())
+	            .trainerId(trainerScheduleDTO.getTrainerId())
+	            .startTime(trainerScheduleDTO.getStartTime())
+	            .endTime(trainerScheduleDTO.getEndTime())
+	        		.build();
+	    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 package fitmeup.entity;
 
