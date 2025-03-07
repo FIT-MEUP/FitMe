ac6ef355bdddb6ae4b25575c7b8d1c59364aa6d3
최용재 <ygr629@gmail.com>
2025-03-07 16:00:55 +0900

    트레이너 어플리케이션

--- src/main/java/fitmeup/repository/TrainerApplicationRepository.java
+++ src/main/java/fitmeup/repository/TrainerApplicationRepository.java
@@ -1,5 +1,7 @@
 package fitmeup.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import fitmeup.entity.TrainerApplicationEntity;
@@ -7,4 +9,6 @@ import fitmeup.entity.TrainerApplicationEntity;
 @Repository
 public interface TrainerApplicationRepository extends JpaRepository<TrainerApplicationEntity, Long> {
     boolean existsByUserUserEmailAndTrainerTrainerId(String userEmail, Long trainerId);
+
+    List<TrainerApplicationEntity> findByTrainerTrainerId(Long trainerNum);
 }
