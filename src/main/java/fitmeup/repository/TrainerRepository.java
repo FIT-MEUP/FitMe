e72e6a513c94d2c83c507d65c9b9c2e3316e2c59
Comet0326 <heasung458@gmail.com>
2025-03-12 16:30:10 +0900

    [feat] 비밀번호 변경 및 임시 비밀번호 발급 기능, 회원탈퇴 기능 구현 (탈퇴기능은 프론트 구성 후 연결 예졍)

--- src/main/java/fitmeup/repository/TrainerRepository.java
+++ src/main/java/fitmeup/repository/TrainerRepository.java
@@ -1,6 +1,7 @@
 package fitmeup.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,6 +14,7 @@ public interface TrainerRepository extends JpaRepository<TrainerEntity, Long> {
 
 	List<TrainerEntity> findByUser_Role(UserEntity.Role role);
 //    List<TrainerEntity> findByTrainerIdAndStatus(Long trainerId, String status);
-
+    // ✅ `trainerId`가 아닌 `userId` 기준으로 트레이너 조회
+    Optional<TrainerEntity> findByUser_UserId(Long userId);
 
 }
--- src/main/java/fitmeup/repository/UserRepository.java
+++ src/main/java/fitmeup/repository/UserRepository.java
@@ -11,13 +11,22 @@ import fitmeup.entity.UserEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUserEmail(String email);
-    Optional<UserEntity> findByUserContact(String contact);
+    Optional<UserEntity> findByUserContact(String userContact);
     List<UserEntity> findByRole(UserEntity.Role role);
-    Optional<UserEntity> findById(Long id);
+    Optional<UserEntity> findById(Long userId);
+    Optional<UserEntity> findByUserNameAndUserEmail(String userName, String userEmail);
     
  // 이름과 연락처가 일치하는 이메일 조회
     @Query("SELECT u.userEmail FROM UserEntity u WHERE u.userName = :userName AND u.userContact = :userContact")
     Optional<String> findEmailByUserNameAndUserContact(@Param("userName") String userName, @Param("userContact") String userContact);
+    
+    @Query("SELECT u FROM UserEntity u WHERE u.userName = :userName AND u.userEmail = :userEmail AND u.userContact = :userContact")
+    Optional<UserEntity> findByUserNameAndUserEmailAndUserContact(@Param("userName") String userName,
+                                                                  @Param("userEmail") String userEmail,
+                                                                  @Param("userContact") String userContact);
+
+
+
 
 
 }
